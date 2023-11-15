8285b95472eae0fc13687411d9a747f143ba414b
KevenAbraham <abraham.keven20@gmail.com>
2023-11-15 14:33:53 -0300

    linkando para o login se clicar que não tem conta

--- app/src/main/java/com/abrahamkeven/netflixo/CadastrarActivity.kt
+++ app/src/main/java/com/abrahamkeven/netflixo/CadastrarActivity.kt
@@ -1,6 +1,8 @@
 package com.abrahamkeven.netflixo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.abrahamkeven.netflixo.ui.theme.NetflixoTheme
 
@@ -8,5 +10,17 @@ class CadastrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastrar)
+
+        val linkToLogin = findViewById<TextView>(R.id.haveAnAccountTextView) //clicar em já tem uma conta
+
+        linkToLogin.setOnClickListener {
+            entrarLogin()
+        }
+    }
+
+    private fun entrarLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish() //destrói essa sessão atual e abre a nova janela (login)
     }
 }
