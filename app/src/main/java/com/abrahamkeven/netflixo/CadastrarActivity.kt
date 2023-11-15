0246faac778846b93cf7ec198b8c4560515cbfa3
KevenAbraham <abraham.keven20@gmail.com>
2023-11-15 14:37:19 -0300

    ir para pagina de escolha de perfil quando clicado

--- app/src/main/java/com/abrahamkeven/netflixo/CadastrarActivity.kt
+++ app/src/main/java/com/abrahamkeven/netflixo/CadastrarActivity.kt
@@ -2,6 +2,7 @@ package com.abrahamkeven.netflixo
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.abrahamkeven.netflixo.ui.theme.NetflixoTheme
@@ -12,6 +13,11 @@ class CadastrarActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cadastrar)
 
         val linkToLogin = findViewById<TextView>(R.id.haveAnAccountTextView) //clicar em já tem uma conta
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            irParaPerfil()
+        }
 
         linkToLogin.setOnClickListener {
             entrarLogin()
@@ -22,5 +28,11 @@ class CadastrarActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish() //destrói essa sessão atual e abre a nova janela (login)
+    }
+
+    private fun irParaPerfil() {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
