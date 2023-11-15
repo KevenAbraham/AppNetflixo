e798d8329ac2f3a0f1e89ed9f60f5770fb17d706
KevenAbraham <abraham.keven20@gmail.com>
2023-11-15 14:42:48 -0300

    funcionalidades e links para login

--- app/src/main/java/com/abrahamkeven/netflixo/LoginActivity.kt
+++ app/src/main/java/com/abrahamkeven/netflixo/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.abrahamkeven.netflixo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.abrahamkeven.netflixo.ui.theme.NetflixoTheme
 
@@ -8,5 +11,26 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val forgot = findViewById<TextView>(R.id.forgotTextView) //esqueci a senha
+        val entrar = findViewById<Button>(R.id.loginButton)
+
+        forgot.setOnClickListener {
+            irParaForget()
+        }
+
+        entrar.setOnClickListener {
+            irParaProfile()
+        }
+    }
+
+    private fun irParaProfile() {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun irParaForget() {
+        val intent = Intent(this, ForgetActivity::class.java)
+        startActivity(intent)
     }
 }
