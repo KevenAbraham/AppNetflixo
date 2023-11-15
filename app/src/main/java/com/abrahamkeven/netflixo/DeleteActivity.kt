41db6b7b6e364e6853a15dd7bcaee59af728db35
KevenAbraham <abraham.keven20@gmail.com>
2023-11-15 14:20:45 -0300

    criação do projeto e das páginas

---
+++ app/src/main/java/com/abrahamkeven/netflixo/DeleteActivity.kt
@@ -0,0 +1,12 @@
+package com.abrahamkeven.netflixo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abrahamkeven.netflixo.ui.theme.NetflixoTheme
+
+class DeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete)
+    }
+}
---
+++ app/src/main/java/com/abrahamkeven/netflixo/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.abrahamkeven.netflixo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.abrahamkeven.netflixo.ui.theme.NetflixoTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnEntrar = findViewById<Button>(R.id.enterButton)
+        val btnCadastrar = findViewById<Button>(R.id.signUpButton)
+
+        btnEntrar.setOnClickListener {
+            entrarLogin()
+        }
+
+        btnCadastrar.setOnClickListener {
+            entrarCadastro()
+        }
+    }
+
+    private fun entrarLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun entrarCadastro() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
